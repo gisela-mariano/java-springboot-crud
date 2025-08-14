1265fd175d6804eb64408485610dde7d0c5eea3e
Gisela Mariano <gisela-mariano@hotmail.com>
2025-08-14 10:57:38 -0300

    first commit

---
+++ src/test/java/java_springboot_crud/JavaSpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package java_springboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
